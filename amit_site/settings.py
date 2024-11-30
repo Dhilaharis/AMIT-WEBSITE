c00e9fd2b74112f52e0c96d10ebe5e85d6cb2bbc
Dhilaharis <dhilaharis48@gmail.com>
2024-11-30 10:56:58 +0530

    NEW

--- amit_site/settings.py
+++ amit_site/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
+# DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
