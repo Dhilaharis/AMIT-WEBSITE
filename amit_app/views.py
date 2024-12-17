cc238a7898c9126c05c7a4d8cf4724d27b2ac2cf
Dhilaharis <dhilaharis48@gmail.com>
2024-12-17 17:30:39 +0530

    last

--- amit_app/views.py
+++ amit_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect,HttpResponse
-
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 def home_page(request):
@@ -19,3 +20,27 @@ def training_page(request):
 
 def contact_page(request):
     return render(request, 'contactus.html')
+
+
+def send_mail_page(request):
+    context = {}
+
+    if request.method == 'POST':
+        
+        subject = request.POST.get('subject')
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        if subject and name and phone and email and message:
+            try:
+                email_body = f"Name: {name}\nPhone: {phone}\nEmail: {email}\n\nMessage:\n{message}"
+                send_mail(subject,email_body, settings.EMAIL_HOST_USER, ['dhilaharis11.abt@gmail.com','dhilaharis.abt@gmail.com'])
+                context['result'] = 'Email sent successfully'
+            except Exception as e:
+                context['result'] = f'Error sending email: {e}'
+        else:
+            context['result'] = 'All fields are required'
+    
+    return render(request, "contactus.html", context)
--- amit_site/settings.py
+++ amit_site/settings.py
@@ -29,7 +29,7 @@ DEBUG = config("DEBUG",cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
-
+# APPEND_SLASH = False
 # Application definition
 
 INSTALLED_APPS = [
@@ -144,3 +144,11 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'your_email@gmail.com'
 EMAIL_HOST_PASSWORD = 'your_email_password'
 DEFAULT_FROM_EMAIL = 'your_email@gmail.com'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dhilaharis.abt@gmail.com'
+EMAIL_HOST_PASSWORD = 'lnej lykd nemb qzbk'
